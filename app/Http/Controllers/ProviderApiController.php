7c834c4be48b9dfdc18e51116ae81bc3bcbb3013
Oscar-CR <krz.oficial@gmail.com>
2023-02-08 09:51:50 -0600

    fix api de principal de proveedor

--- app/Http/Controllers/ProviderApiController.php
+++ app/Http/Controllers/ProviderApiController.php
@@ -12,37 +12,53 @@ use Illuminate\Support\Facades\DB;
 class ProviderApiController extends Controller
 {
     public function providerOrders(Request $request){
-
-        $provider_data = [];
+        
+        $request->validate([
+            'token' => 'required',
+        ]);
+        
         $token_id =  DB::table('personal_access_tokens')->where('token', $request->token)->value('tokenable_id');  
         $user = User::all()->where('id', $token_id)->first();
         $company = Companies::all()->where('id',$user->company_id)->first();
-        $orders = Order::all()->where('provider_name',$company->social_reason)->all();
 
+        $product_data = [];
+        $order_data = [];
+        
+        $orders = Order::all()->where('provider_name',$company->social_reason);
+       
         foreach($orders as $order){
+            
             $products = Product::all()->where('pucharse_order_id',$order->id);
-            array_push($provider_data, (object)[
+           
+            foreach($products as $product){                        
+                    array_push($product_data, (object)[
+                        'data' => $product,
+                    ]);
+                }
+                
+            array_push($order_data, (object)[
                 'id' => $order->id,
-                'code_sale'=> $order->code_sale,
-                'type_purchase'=> $order->type_purchase,
-                'sequence'=> $order->sequence,
-                'company'=> $order->company,
-                'code_purchase'=> $order->code_purchase,
-                'order_date'=> $order->order_date,
-                'provider_name'=> $order->provider_name,
-                'provider_address'=> $order->provider_address,
-                'planned_date'=> $order->planned_date,
-                'supplier_representative'=> $order->supplier_representative,
-                'total'=> $order->total,
-                'status'=> $order->status,
-                'invoice'=> $order->invoice,
-                'xml'=> $order->xml,
+                'code_sale' => $order->code_sale,
+                'type_purchase' => $order->type_purchase,
+                'sequence' => $order->sequence,
+                'company' => $order->company,
+                'code_purchase' => $order->code_purchase,
+                'order_date' => $order->order_date,
+                'provider_name' => $order->provider_name,
+                'provider_address' => $order->provider_address,
+                'planned_date' => $order->planned_date,
+                'supplier_representative' => $order->supplier_representative,
+                'total' => $order->total,
+                'status' => $order->status,
+                'invoice' => $order->invoice,
+                'xml' => $order->xml,
                 'payment_status'=> $order->payment_status,
-                'products'=> $products
+                'product' => $product_data
             ]);
+            $product_data = [];
         }
-        return $provider_data;
-
+       
+        return $order_data;
 
     }
 }
