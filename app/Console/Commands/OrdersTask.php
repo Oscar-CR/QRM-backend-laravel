0784a7fe8bc1cffd82f639055786723fc8dc707e
Oscar-CR <krz.oficial@gmail.com>
2023-02-17 14:47:34 -0600

    agregado de rutas web

--- app/Console/Commands/OrdersTask.php
+++ app/Console/Commands/OrdersTask.php
@@ -33,7 +33,7 @@ class OrdersTask extends Command
     {
         $page = 1;
 
-        $token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2Rldi1hcGktYnBtcy5wcm9tb2xpZmUubGF0L2FwaS9sb2dpbiIsImlhdCI6MTY3NjM4NzU0NSwiZXhwIjoxNjc2NjQ2NzQ1LCJuYmYiOjE2NzYzODc1NDUsImp0aSI6IlVlMVFSSFJnMklFYWpQblQiLCJzdWIiOiI3MCIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjciLCJyb2xlIjpbXSwidXNlciI6eyJuYW1lIjoiSXZvbm5lIEzDs3BleiBFc2NvYmVkbyIsImVtYWlsIjoiaXZvbm5lLmxvcGV6QHByb21vbGlmZS5jb20ubXgiLCJwaG90byI6Imh0dHBzOi8vaW50cmFuZXQucHJvbW9saWZlLmxhdC9zdG9yYWdlL3Bvc3QvMTUuLSUyMEl2b25uZSUyMExvcGV6LmpwZyJ9fQ.h0qGyzUzH-sNch6yhXKFhxHosO4Uqxwl4oVyhjlMX9s';
+        $token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2Rldi1hcGktYnBtcy5wcm9tb2xpZmUubGF0L2FwaS9sb2dpbiIsImlhdCI6MTY3NjY1NjgwNiwiZXhwIjoxNjc2OTE2MDA2LCJuYmYiOjE2NzY2NTY4MDYsImp0aSI6IldHbEJzc1VYOVdSdXhPaDciLCJzdWIiOiI3MCIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjciLCJyb2xlIjpbXSwidXNlciI6eyJuYW1lIjoiSXZvbm5lIEzDs3BleiBFc2NvYmVkbyIsImVtYWlsIjoiaXZvbm5lLmxvcGV6QHByb21vbGlmZS5jb20ubXgiLCJwaG90byI6Imh0dHBzOi8vaW50cmFuZXQucHJvbW9saWZlLmxhdC9zdG9yYWdlL3Bvc3QvMTUuLSUyMEl2b25uZSUyMExvcGV6LmpwZyJ9fQ.6gMb2fmXwNyeiMWfnGVf8LzANNk40tbW7KsJfaTid90';
         $init_url = 'https://dev-api-bpms.promolife.lat/api/pedidos?page='.$page.'&token='. $token;
         $init_ch = curl_init();
         curl_setopt($init_ch, CURLOPT_URL, $init_url);
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminApiController;
+use App\Http\Controllers\GlobalApiController;
+use App\Http\Controllers\ProviderApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,3 +37,29 @@ Route::get('/test-update-files', [TestController::class, 'testUpdateFiles'])->na
 Route::post('/test-update-files-store', [TestController::class, 'testUpdateFilesStore'])->name('test.update-files-store');
 
 require __DIR__.'/auth.php';
+
+
+/*  GLOBAL ROUTES */
+Route::post('/login-user', [GlobalApiController::class, 'loginUser'])->name('api.loginUser'); 
+Route::post('/send-mail', [GlobalApiController::class, 'sendMail'])->name('api.send-mail');
+
+/* ADMIN ROUTES */
+Route::get('/allOrders/{token}', [AdminApiController::class, 'allOrders'])->name('api.allOrders');
+Route::get('/allOrdersByCompany/{token}', [AdminApiController::class, 'allOrdersByCompany'])->name('api.allOrdersByCompany');
+Route::get('/allUsers', [AdminApiController::class, 'allUsers'])->name('api.allUsers');
+Route::get('/requiredUserData', [AdminApiController::class, 'requiredUserData'])->name('api.requiredUserData');
+Route::get('/editUser/{user_id}', [AdminApiController::class, 'editUser'])->name('api.editUser');
+Route::post('/updateUser', [AdminApiController::class, 'updateUser'])->name('api.updateUser');
+Route::post('/deleteUser', [AdminApiController::class, 'deleteUser'])->name('api.deleteUser');
+Route::get('/general', [AdminApiController::class, 'general'])->name('api.general');
+/* Route::post('/updateXML', [AdminApiController::class, 'updateXML'])->name('api.updateXML');
+Route::post('/updatePDF', [AdminApiController::class, 'updatePDF'])->name('api.updatePDF'); */
+Route::post('/generalInitialInvoinces', [AdminApiController::class, 'generalInitialInvoinces'])->name('api.generalInitialInvoinces');
+Route::post('/invoicesByDate', [AdminApiController::class, 'invoicesByDate'])->name('api.invoicesByDate');
+
+/* PROVIDER ROUTES */
+Route::get('/providerOrders/{token}', [ProviderApiController::class, 'providerOrders'])->name('api.providerOrders');
+Route::post('/updateFiles', [ProviderApiController::class, 'updateFiles'])->name('api.updateFiles');
+
+/* TEST */
+Route::post('/testOrders', [TestController::class, 'testOrders'])->name('api.testOrders');
